1efad59cb98e0c8d14e769fd275089bedb391ade
Lucas Caetano <lucascaetanocosta@gmail.com>
2022-09-22 20:33:09 -0300

    Aula: 14

---
+++ Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/TiposPrimitivos/TiposPrimitivosExercicio.java
@@ -0,0 +1,13 @@
+package MaratonaJavaViradoNoJiraya.TiposPrimitivos;
+
+public class TiposPrimitivosExercicio {
+    public static void main(String[] args) {
+        String name= "Ichigo";
+        String address = "Karakura";
+        float wage = 1526.84F;
+        String date = "10/15";
+
+        System.out.println("Eu " + name + ", morando no endereço " + address
+                + ", confirmo que recebi o salário de " + wage + ", na data " + date + ".");
+    }
+}

1819f8c92ce7f1879992e353d39b2b8df0af129d
Lucas Caetano <lucascaetanocosta@gmail.com>
2022-09-25 08:22:44 -0300

    Aula: 15 :ninja: :wood:

--- Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/Operadores/Operadores01.java
+++ Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/Operadores/Operadores01.java
@@ -1,25 +1,22 @@
 package MaratonaJavaViradoNoJiraya.Operadores;
 
-/*
-Operadores básicos:
--Soma: +
--Subtração: -
--Divisão: /
--Multiplicação: *
- */
-
 public class Operadores01 {
+    /**
+     * <b>Operadores básicos</b>:<br>
+     * Soma (+), Subtração (-), Divisão (/), Multiplicação (*)
+     */
     public static void main(String[] args) {
 
-        int numero1 = 10;
-        int numero2 = 5;
-        double resultado = numero1 / (double) numero2;
-        boolean isResultadoMairo = numero1 > numero2;
-        boolean isResultadoMenor = numero1 < numero2;
+        int number1 = 13;
+        int number2 = 5;
+        double result = number1 / (double) number2;
+        boolean isBiggestResult = number1 > number2;
+        boolean isSmallestResult = number1 < number2;
+
+        System.out.println(result);
+        System.out.println(isBiggestResult);
+        System.out.println(isSmallestResult);
 
 
-        System.out.println(resultado);
-        System.out.println(isResultadoMairo);
-        System.out.println(isResultadoMenor);
     }
 }

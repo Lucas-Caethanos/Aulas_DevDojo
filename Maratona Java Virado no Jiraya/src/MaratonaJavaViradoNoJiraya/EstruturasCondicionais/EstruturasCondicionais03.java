9982cf75a945403f53f036a3fc38158fbfe2cb52
Lucas Caetano <lucascaetanocosta@gmail.com>
2022-09-26 19:04:57 -0300

    Aula: 22 :ninja: :wood:

--- Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/EstruturasCondicionais/EstruturasCondicionais03.java
+++ Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/EstruturasCondicionais/EstruturasCondicionais03.java
@@ -3,16 +3,16 @@ package MaratonaJavaViradoNoJiraya.EstruturasCondicionais;
 
 public class EstruturasCondicionais03 {
     public static void main(String[] args) {
-        //Doar se salario > 5000;
-        double salario = 4000;
+        //Donate if wage > 5000;
+        double wage = 4000;
 
-        //String mensagemDoar = "Eu vou doar";
-        //String mensagemNaoDoar = "Eu não vou doar";
+        //String donationMenssage = "Eu vou doar";
+        //String nonDonateMenssage = "Eu não vou doar";
 
         //Operador ternário --> (condicao) ? verdadeiro : falso
-        String resultado = salario > 5000 ? "Eu vou doar" : "Eu não vou doar";
+        String resultado = wage > 5000 ? "Eu vou doar" : "Eu não vou doar";
 /*
-        if(salario > 5000){
+        if(wage > 5000){
             resultado = mensagemDoar;
         }else{
             resultado = mensagemNaoDoar;

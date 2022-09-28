51a27b3729f9a4f3a313c26028e13ea9544bf9b3
Lucas Caetano <lucascaetanocosta@gmail.com>
2022-09-28 18:09:42 -0300

    Aula: 80 :ninja: :wood:

---
+++ Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/OrientacaoObjetos/Javacore/Enumeracao/Dominio/TipoCliente.java
@@ -0,0 +1,11 @@
+package MaratonaJavaViradoNoJiraya.OrientacaoObjetos.Javacore.Enumeracao.Dominio;
+
+/**
+ * Nessa calsse os atributos são constantes<br>
+ * PESSOA_FISICA<br>
+ * PESSOA_JURIDICA
+ */
+public enum TipoCliente {
+    PESSOA_FISICA,
+    PESSOA_JURIDICA
+}

ac1fb36f2e9adb1656167cc9f4bbd7d965bbcfd0
Lucas Caetano <lucascaetanocosta@gmail.com>
2022-10-05 11:41:24 -0300

    Aula: 97 :ninja: :wood:

---
+++ Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/Excecoes/exception/test/ExceptionTest01.java
@@ -0,0 +1,24 @@
+package MaratonaJavaViradoNoJiraya.Excecoes.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest01 {
+    public static void main(String[] args) {
+        criarNovoArquivo();
+
+    }
+
+    private static void criarNovoArquivo() {
+        File file = new File("arquivo\\teste.txt");
+        try {
+            boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado: " + isCriado);
+
+        } catch (IOException e) {
+            //Nunca deixe esse bloco em branco
+            e.printStackTrace();
+
+        }
+    }
+}

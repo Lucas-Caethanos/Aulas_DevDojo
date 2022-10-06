fe5bfbd2310755a1d88a833b40dfe6d47384839c
Lucas Caetano <lucascaetanocosta@gmail.com>
2022-10-06 10:04:07 -0300

    Aula: 103 :ninja: :wood:

---
+++ Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/Excecoes/exception/test/TryWithResourcesTest01.java
@@ -0,0 +1,38 @@
+package MaratonaJavaViradoNoJiraya.Excecoes.exception.test;
+
+import MaratonaJavaViradoNoJiraya.Excecoes.exception.dominio.Leitor1;
+import MaratonaJavaViradoNoJiraya.Excecoes.exception.dominio.Leitor2;
+
+import java.io.*;
+
+public class TryWithResourcesTest01 {
+    public static void main(String[] args) {
+        lerArquivo();
+        lerArquivo2();
+    }
+    public static void lerArquivo(){
+        try (Leitor1 leitor1 = new Leitor1();
+             Leitor2 leitor2 = new Leitor2()){
+
+        }catch (IOException e){
+//            e.printStackTrace();
+        }
+
+    }
+    public static void lerArquivo2(){
+        Reader reader = null;
+        try {
+            reader = new BufferedReader(new FileReader("teste.txt"));
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+}

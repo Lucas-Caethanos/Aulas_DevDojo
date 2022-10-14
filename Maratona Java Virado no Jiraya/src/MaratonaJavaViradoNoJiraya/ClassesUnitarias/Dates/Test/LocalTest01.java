4e3819e0a209f144b4a7e1456be3cdb121682716
Lucas Caetano <lucascaetanocosta@gmail.com>
2022-10-14 17:26:57 -0300

    Aula: 115 :ninja: :wood:

---
+++ Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/ClassesUnitarias/Dates/Test/LocalTest01.java
@@ -0,0 +1,20 @@
+package MaratonaJavaViradoNoJiraya.ClassesUnitarias.Dates.Test;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class LocalTest01 {
+    public static void main(String[] args) {
+        //pt-BR
+        Locale localeJapão = new Locale("ja", "JP");
+        Locale localeCH = new Locale("it", "CH");
+        Calendar calendar = Calendar.getInstance();
+        DateFormat df1 = DateFormat.getDateInstance(DateFormat.FULL,localeJapão);
+        DateFormat df2 = DateFormat.getDateInstance(DateFormat.FULL,localeCH);
+        System.out.println("Japão: " + df1.format(calendar.getTime()));
+        System.out.println(localeJapão.getDisplayCountry(localeJapão));
+        System.out.println("Suiça: " + df2.format(calendar.getTime()));
+        System.out.println(localeCH.getDisplayCountry(localeCH));
+    }
+}

e1aa7056a66b2661104cad2f786f9e49714d85e7
Lucas Caetano <lucascaetanocosta@gmail.com>
2022-10-14 18:20:01 -0300

    Aula: 119 :ninja: :wood:

---
+++ Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/ClassesUnitarias/Dates/Test/LocalDateTest01.java
@@ -0,0 +1,18 @@
+package MaratonaJavaViradoNoJiraya.ClassesUnitarias.Dates.Test;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.Calendar;
+import java.util.Date;
+
+public class LocalDateTest01 {
+    public static void main(String[] args) {
+        System.out.println(new Date());
+        System.out.println(Calendar.getInstance());
+        LocalDate date = LocalDate.of(2022, Month.JANUARY,27);
+        System.out.println(date.getMonth());
+        System.out.println(date.getYear());
+        System.out.println(date.getDayOfMonth());
+
+    }
+}

de0551f7a63c80af1f6e3954f30bb316e282f18f
Lucas Caetano <lucascaetanocosta@gmail.com>
2022-10-14 17:58:39 -0300

    Aula: 117 :ninja: :wood:

---
+++ Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/ClassesUnitarias/FormatacaoTest/NumberFprmatTest02.java
@@ -0,0 +1,28 @@
+package MaratonaJavaViradoNoJiraya.ClassesUnitarias.FormatacaoTest;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+
+public class NumberFprmatTest02 {
+    public static void main(String[] args) {
+        Locale localeDefault = Locale.getDefault();
+        Locale localePT = new Locale("pt", "BR");
+        Locale localeJP = Locale.JAPAN;
+        Locale localeIT = Locale.ITALY;
+
+        NumberFormat[] nfa = new NumberFormat[4];
+        nfa[0] = NumberFormat.getCurrencyInstance();
+        nfa[1] = NumberFormat.getCurrencyInstance(localeJP);
+        nfa[2] = NumberFormat.getCurrencyInstance(localeIT);
+        nfa[3] = NumberFormat.getCurrencyInstance(localePT);
+
+        double valor = 100_000.2130;
+
+        for (NumberFormat numberFormat : nfa) {
+            System.out.println(numberFormat.format(valor));
+            System.out.println();
+        }
+
+
+    }
+}

68ea9a2e6db0c6be152f565cdaa05a8038938d09
Lucas Caetano <lucascaetanocosta@gmail.com>
2022-10-15 10:10:50 -0300

    Aula: 126 :ninja: :wood:

---
+++ Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/ClassesUnitarias/Dates/Test/TemporalAdjustersTest01.java
@@ -0,0 +1,14 @@
+package MaratonaJavaViradoNoJiraya.ClassesUnitarias.Dates.Test;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoField;
+
+public class TemporalAdjustersTest01 {
+    public static void main(String[] args) {
+        LocalDate now = LocalDate.now();
+        now = now.with(ChronoField.DAY_OF_MONTH, 20);
+        System.out.println(now);
+        System.out.println(now.getDayOfWeek());
+
+    }
+}

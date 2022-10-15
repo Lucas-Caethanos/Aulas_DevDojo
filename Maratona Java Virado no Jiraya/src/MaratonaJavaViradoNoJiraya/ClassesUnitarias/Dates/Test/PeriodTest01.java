c2c1be683c0b8998eb408e3f4f50250a68d62f26
Lucas Caetano <lucascaetanocosta@gmail.com>
2022-10-15 09:53:54 -0300

    Aula: 125 :ninja: :wood:

--- Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/ClassesUnitarias/Dates/Test/PeriodTest01.java
+++ Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/ClassesUnitarias/Dates/Test/PeriodTest01.java
@@ -2,6 +2,7 @@ package MaratonaJavaViradoNoJiraya.ClassesUnitarias.Dates.Test;
 
 import java.time.LocalDate;
 import java.time.Period;
+import java.time.temporal.ChronoUnit;
 
 public class PeriodTest01 {
     public static void main(String[] args) {
@@ -10,10 +11,17 @@ public class PeriodTest01 {
         Period p1 = Period.between(now,nowAfterTwoYears);
         Period p2 = Period.ofDays(10);
         Period p3 = Period.ofWeeks(58);
+        Period p4 = Period.ofMonths(3);
+        Period p5 = Period.ofYears(3);
 
 
         System.out.println(p1);
         System.out.println(p2);
         System.out.println(p3);
+        System.out.println(p4);
+        System.out.println(p5);
+        System.out.println(p3.getMonths());
+        System.out.println(Period.between(now, now.plusDays(p3.getDays())).getMonths());
+        System.out.println(now.until(now.plusDays(p3.getDays()), ChronoUnit.DAYS));
     }
 }

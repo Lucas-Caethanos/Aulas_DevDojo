7c63bd1f60f152175f41646b6d18eed1cdb7e1ea
Lucas Caetano <lucascaetanocosta@gmail.com>
2022-10-16 17:31:15 -0300

    Aula: 132 :ninja: :wood:

--- Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/ClassesUnitarias/Regex/Test/PatternMatcherTest02.java
+++ Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/ClassesUnitarias/Regex/Test/PatternMatcherTest02.java
@@ -3,11 +3,14 @@ package MaratonaJavaViradoNoJiraya.ClassesUnitarias.Regex.Test;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class PatternMatcherTest01 {
+public class PatternMatcherTest02 {
     public static void main(String[] args) {
-        String regex = "aba";
-        String texto = "abaaba";
-        String texto2 = "abababa";
+        //String regex = "\\d"; // \d -> buca todos os dígitos
+        //String regex = "\\D"; // \d -> o que não for dígito (não número)
+        String regex = "\\s"; // \s espaços em branco
+
+        //   String texto = "aba";
+        String texto2 = "hhj$2 12&gvh21_k h2!";
         Pattern pattern = Pattern.compile(regex); //Define um padrão (para ser usado em uma pesquisa)
         Matcher matcher = pattern.matcher(texto2); //Usada para procurar o padrão
 

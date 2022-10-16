9dbaa0c714347ae847147227d2d61af9e1644440
Lucas Caetano <lucascaetanocosta@gmail.com>
2022-10-16 19:19:25 -0300

    Aula: 137 :ninja: :wood:

---
+++ Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/ClassesUnitarias/Regex/Test/ScannerTest01.java
@@ -0,0 +1,14 @@
+package MaratonaJavaViradoNoJiraya.ClassesUnitarias.Regex.Test;
+
+public class ScannerTest01 {
+    public static void main(String[] args) {
+        String texto = "Levi, Eren, Mikasa";
+        String[] nomes = texto.split(",");
+
+        for (String nome : nomes) {
+            System.out.println(nome.trim());
+        }
+
+
+    }
+}

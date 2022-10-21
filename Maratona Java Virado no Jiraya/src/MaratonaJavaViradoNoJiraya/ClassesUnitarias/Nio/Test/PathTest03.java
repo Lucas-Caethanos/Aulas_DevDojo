e6058c5c7174672ad17df577ccd4878b110b365a
Lucas Caetano <lucascaetanocosta@gmail.com>
2022-10-21 17:09:49 -0300

    Aula: 147 :ninja: :wood:

---
+++ Maratona Java Virado no Jiraya/src/MaratonaJavaViradoNoJiraya/ClassesUnitarias/Nio/Test/PathTest03.java
@@ -0,0 +1,9 @@
+package MaratonaJavaViradoNoJiraya.ClassesUnitarias.Nio.Test;
+
+import java.nio.file.Paths;
+
+public class PathTest03 {
+    public static void main(String[] args) {
+        //
+    }
+}
